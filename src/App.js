ad9281c9c618ab7ef80e7c90446969cd278b65f2
Spencer Yeboah-Mensah <spencer.mensah98@gmail.com>
2018-12-22 19:18:05 +0000

    First Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'aframe-animation-component'
+import {Entity} from 'aframe-react';
+import { AFrameRenderer, Marker} from 'react-web-ar'
+
+class App extends Component {
+  render() {
+    //mtl: #tree-mtl
+    // <Entity
+    //   position={{ x: 0, y: 0, z: 0 }}
+    //   obj-model='obj: logo.obj; mtl: logo.mtl;'
+    //   animation="property: rotation; to: 0 0 360; loop: true; dur: 5000"
+    //   scale={{x: 0.3, y: 0.3, z: 0.3}}
+    //   />
+    return (
+        <AFrameRenderer inherent={true}>
+          <Marker parameters={{ preset: "hiro" }}>
+            <Entity
+              position={{ x: 0, y: 0, z: 0 }}
+              obj-model='obj: trousers.obj; mtl: trousers.mtl;'
+              animation="property: rotation; to: 0 0 360; loop: true; dur: 5000"
+              scale={{x: 0.3, y: 0.3, z: 0.3}}
+              />
+          </Marker>
+        </AFrameRenderer>
+    );
+  }
+}
+
+export default App;
---
+++ src/rootAssets.js
@@ -0,0 +1,4 @@
+export default {
+  // [ComponentName:string]: Your declared Assets array
+  yourmum: require('./yourmum.obj').default.Assets
+};
