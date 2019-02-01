db8587604d85916d1edef74200c00b2582bd8796
Spencer Yeboah-Mensah <spencer.mensah98@gmail.com>
2019-02-01 16:26:29 +0000

    For Business Card

--- src/models/trousers/Trousers.js
+++ src/models/trousers/Trousers.js
@@ -11,6 +11,11 @@ class Trousers extends Component {
           position={{ x: 0, y: 0, z: 0 }}
           obj-model='obj: Bbackandfront.obj; mtl: Bbackandfront.mtl;'
           animation="property: rotation; to: 0 0 360; loop: true; dur: 6000"
+          scale={{x: 0.2, y: 0.2, z: 0.2}}
+          />
+        <Entity
+          position={{ x: 0, y: 0, z: -1.2 }}
+          obj-model='obj: LogoText.obj; mtl: LogoText.mtl;'
           scale={{x: 0.3, y: 0.3, z: 0.3}}
           />
       </Marker>
