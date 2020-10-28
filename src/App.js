e2579d5bcf439a9074c62e675717a9b6a7337ef8
Spencer Yeboah-Mensah <blackaroly@gmail.com>
2020-10-28 20:16:39 +0000

    added new model

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react';
 import 'aframe-animation-component'
 import {AFrameRenderer} from 'react-web-ar'
 import Logo from './models/logo/Logo'
-import Trousers from './models/trousers/Trousers'
 import AlertDialog from './modal'
 import FaInstagram from 'react-icons/lib/fa/instagram';
 import FaShoppingBasket from 'react-icons/lib/fa/shopping-basket';
+
 
 
 class App extends Component {
   constructor(props) {
       super(props);
 }
+
+
 
 
   render() {
--- src/models/logo/Logo.js
+++ src/models/logo/Logo.js
@@ -5,12 +5,10 @@ import {Marker} from 'react-web-ar'
 class Logo extends Component {
   render() {
     return (
-      <Marker parameters={{ preset: 'custom', type: 'pattern', patternUrl: 'pieceslogo.patt'}}>
+      <Marker parameters={{ preset: 'custom', type: 'pattern', patternUrl: 'newN.patt'}}>
         <Entity
-          position={{ x: -0.5, y: 0, z: 0 }}
-          obj-model='obj: logostd.obj; mtl: logostd.mtl;'
-          animation="property: rotation; to: 360 0 0; loop: true; dur: 5000"
-          scale={{x: 0.15, y: 0.15, z: 0.15}}
+          position={{ x: 0, y: 0, z: 0 }}
+          gltf-model='slide.gltf'
           />
       </Marker>
     );
